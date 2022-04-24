382fb8604b5194b09e7d025d44ec61e702e28aa1
Jarret Rose <jarretrose@gmail.com>
2022-04-24 08:49:25 -0400

    init

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Serve React
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Routes
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+// Catch all other requests
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, '../client/build', 'index.html')
+  );
+});
+
+// Listen
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
