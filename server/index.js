ab1df4a3a0b9c35e93cccd5b7058487a893f7c72
Jarret Rose <jarretrose@gmail.com>
2022-05-02 23:07:23 -0400

    test

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Routes
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
+  res.json({ message: 'Hello World' });
 });
 
 // Catch all other requests
