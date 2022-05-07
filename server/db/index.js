a92ca17190dd243ab2b5ffdb98fbdf6dd5a62990
Jarret Rose <jarretrose@gmail.com>
2022-05-07 15:39:36 -0400

    starting psql imp

---
+++ server/db/index.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+});
+
+db.authenticate()
+  .then(() => console.log('Successfully connected to the database.'))
+  .catch((err) =>
+    console.log('Error connecting to the database.', err)
+  );
+
+module.exports = db;
