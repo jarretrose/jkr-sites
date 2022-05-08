d699fcdec6192047fd643b1741f888670253bd00
Jarret Rose <jarretrose@gmail.com>
2022-05-07 20:00:43 -0400

    able to connect to db

--- server/db/index.js
+++ server/db/index.js
@@ -2,20 +2,12 @@ require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
-const { DB_DATABASE, DB_USERNAME, DB_PASSWORD, DB_HOST } =
-  process.env;
-
-const db = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: 'postgres',
-});
+const db = new Sequelize(process.env.DATABASE_URL);
 
 db.authenticate()
   .then(() => console.log('Successfully connected to the database.'))
   .catch((err) =>
     console.log('Error connecting to the database.', err)
   );
-
-// const db = {};
 
 module.exports = db;
