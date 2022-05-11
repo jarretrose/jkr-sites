ccb9b41f59fc81d53489ae886fc062fb3b890b69
Jarret Rose <jarretrose@gmail.com>
2022-05-11 13:13:36 -0400

    adding db

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const path = require('path');
 const express = require('express');
-const db = require('./db');
+const db = require('../db');
 
 const PORT = process.env.PORT || 3001;
 
@@ -27,50 +27,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-/*
-
-const dotenv = require('dotenv');
-dotenv.config();
-
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan')
-const session = require('express-session');
-const jwt = require('jwt-simple')
-const bcrypt = require('bcrypt')
-
-const db = require('./db')
-const api = require('./api');
-
-const app = express();
-
-app.use(morgan('tiny'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Session middleware, because my JWT auth broke things and so I am using sessions in the meantime
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-}))
-
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use('/dist', express.static(path.join(__dirname, '..', 'dist')));
-app.get('/', (req, res) => res.sendFile(index.html));
-app.use('/api', api);
-
-// custom error handler
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: req.app.get('env') === 'development' ? err : {},
-  });
-});
-
-
-module.exports = app;
-
-*/
