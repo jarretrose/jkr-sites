8489a40b0348c2fd6356f7bddd9a71ee24074518
Jarret Rose <jarretrose@gmail.com>
2022-05-24 20:52:31 -0400

    sync

--- db/index.js
+++ db/index.js
@@ -6,8 +6,6 @@ const db = new Sequelize(process.env.DATABASE_URL);
 
 db.authenticate()
   .then(() => console.log('Successfully connected to the database.'))
-  .catch((err) =>
-    console.log('Error connecting to the database.', err)
-  );
+  .catch((err) => console.log('Error connecting to the database.'));
 
 module.exports = db;
